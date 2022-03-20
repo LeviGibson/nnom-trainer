743f67811afdbdb73e5becdf458172ee3b0098dc
LeviGibson <72767501+LeviGibson@users.noreply.github.com>
2022-03-20 09:35:51 -0400

    updating feature generation to work with the new architecture

--- labels.py
+++ labels.py
@@ -1,11 +1,15 @@
 import chess
 import numpy as np
-from halfkp import flipPers, piece_to_ordinal
+from halfkp import flipPers, piece_to_ordinal, flip_piece_pers
 
 def generate_labels(move : chess.Move, board : chess.Board):
     label = np.zeros((384,), bool)
     p = piece_to_ordinal(board.piece_at(move.from_square))
     sq = flipPers[move.to_square]
+
+    if board.turn == chess.BLACK:
+        p = flip_piece_pers[p]
+        sq = flipPers[sq]
 
     label[(p*64) + (sq)] = True
     return label
